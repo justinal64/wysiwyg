1b98a7d1bec02add2a120407d1c740633ae9a3ea
justin leggett <justin@codingrookie.com>
2016-08-24 10:52:57 -0500

    completed project and removed comments

--- js/wysiwyg.js
+++ js/wysiwyg.js
@@ -45,9 +45,11 @@ var famousPeople = [{
         death: 1797
     }
 }];
+
 // used to iterate through object
 var i = 0;
 var lastSection;
+var editBio = document.getElementById("editBio");
 console.log(famousPeople);
 
 
@@ -62,18 +64,13 @@ for (var counter = 0; counter < 5; counter++) {
   i++;
 }
 
-function editBioListener(editBio, bio) {
-        editBio.value = bio.innerHTML;
-        // document.getElementById("editBio").addEventListener("keyup", function(event) {
-        //     console.log(event);
-        // })
-}
-
 // keyup eventListener for the textarea
 document.getElementById("editBio").addEventListener("keyup", function(event) {
-    console.log("lastSection", lastSection);
-    document.getElementById(lastSection).innerHTML = document.getElementById("editBio").value;
-    console.log(document.getElementById("editBio").value);
+    if(event.keyCode === 13) {
+        editBio.value = "";
+    } else {
+        document.getElementById(lastSection).innerHTML = editBio.value;
+    }
 });
 
 // Now containerEl will have elements in it
@@ -82,26 +79,24 @@ var containerEl = document.getElementsByClassName("person_container");
 // Event listeners are created
 for (var i = 0; i < containerEl.length; i++) {
   containerEl[i].addEventListener("click", function (event) {
-        /*move everything in the bio field to the input field and any
-        change you make should appear in the bio that you clicked.*/
         // This gets the innerHTML of the section element
         var bio = document.getElementById(this.id).getElementsByTagName("section")[0];
+        // set border of the section you clicked
+        document.getElementById(bio.id).style.border = "4px dotted blue";
+        // sets focus to the input field
+        editBio.focus();
+        // used when the keyup function sends the data back to the section element
         lastSection = bio.id;
-        console.log(lastSection);
-        var editBio = document.getElementById("editBio");
-        editBioListener(editBio, bio);
+
+        editBio.value = bio.innerHTML;
     });
 };
 
 
-/*1. Create an array of objects that represents famous people (see structure below). Done
-2. Create a text input in your DOM. Done
-3. Beneath that, create a container, block element in your DOM.
-4. Create a DOM element for each of the objects inside the container. Style your person elements however you like. Done
-5. For every even numbered element, have a light yellow background. Done
-6. For every odd numbered element, have a light blue background. Done
-7. Each element's DOM structure should be as shown below. Done
-8. When you click on one of the person elements, a dotted border should appear around it.
-9. When you click on one of the person elements, the text input should immediately gain focus so that you can start typing.
-10. When there is a highlighted person element, and you begin typing in the input box, the person's biography should be immediately bound to what you are typing, letter by letter.
-11. When you press the enter/return key when typing in the input field, then the content of the input field should immediately be blank.*/
+
+
+
+
+
+
+
